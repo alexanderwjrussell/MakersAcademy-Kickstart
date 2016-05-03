0c954ef3fcbe405144ca71a7b28435bcc14ecdd9
alexanderwjrussell <awjrussell@live.co.uk>
2016-05-03 20:33:17 +0100

    Starting challenge 5

---
+++ Session 2/Challenges/5_array.rb
@@ -0,0 +1,13 @@
+def mod_three(numbers)
+
+
+end
+
+puts mod_three [0]
+puts mod_three [1]
+puts mod_three [2]
+puts mod_three [3]
+puts mod_three [4]
+puts mod_three [5]
+puts mod_three [6]
+puts mod_three [7]
