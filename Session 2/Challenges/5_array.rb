8fa6cffaee3f11269709fec978d9f0fb5a15472d
alexanderwjrussell <awjrussell@live.co.uk>
2016-05-03 20:49:02 +0100

    Challenge 5 solved
    After a few modifications and changes in logic. A solution has been
    found!

--- Session 2/Challenges/5_array.rb
+++ Session 2/Challenges/5_array.rb
@@ -1,7 +1,10 @@
 def mod_three(numbers)
-
-
+ numbers.select do |n|
+  n = n % 3
+  puts n
+  end
 end
+
 
 puts mod_three [0]
 puts mod_three [1]
@@ -11,3 +14,4 @@ puts mod_three [4]
 puts mod_three [5]
 puts mod_three [6]
 puts mod_three [7]
+puts mod_three [1,2,3,4,5,6,7]
